b584483e5d15b067a688b34663d21895507af107
BowlPulp <kartikarora5832@gmail.com>
2025-03-06 12:31:13 +0530

    port change

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ connectDB();
 // Use user routes at a specific endpoint (e.g., /api/users)
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`Server is running on port ${PORT}`);
 });
