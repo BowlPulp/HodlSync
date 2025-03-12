180970664866bfb49441ca4632a99e4615e9bd65
BowlPulp <kartikarora5832@gmail.com>
2025-03-12 10:38:50 +0530

    Cors trailing dash error

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-  origin: "https://www.hodlsync.com/",
+  origin: "https://www.hodlsync.com",
   credentials: true // Allows cookies in requests
 }));
 
