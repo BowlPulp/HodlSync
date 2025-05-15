97aa8a529ceccace719ccef07f0c627720412031
BowlPulp <kartikarora5832@gmail.com>
2025-05-15 08:56:12 +0530

    Add @vercel/analytics dependency and initialize in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,9 @@ import './index.css';
 import App from './App.jsx';
 import { ToastContainer } from 'react-toastify';
 import ScrollToTop from './components/ScrollToTop.jsx';
+import { inject } from '@vercel/analytics'
 
+inject()
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>   {/* Wrap your App component with BrowserRouter */}
